7fd857763eb3e8e43fa825aa3abe9c3dc9c64aeb
Wahaj Hussain <wahajhssn@gmail.com>
2020-05-02 03:43:38 -0700

    IAP

--- App.js
+++ App.js
@@ -1,90 +1,140 @@
-import { AppLoading } from "expo";
+// import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import React, { useState, useEffect } from "react";
 import {
-	Platform,
-	StatusBar,
-	StyleSheet,
-	View,
-	AsyncStorage
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  AsyncStorage,
+  Image,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import WebView from "react-native-webview";
 import * as FileSystem from "expo-file-system";
 import { AdMobInterstitial } from "expo-ads-admob";
+import * as InAppPurchases from "expo-in-app-purchases";
 
 import AppNavigator from "./navigation/AppNavigator";
 
 export default function App(props) {
-	const [isLoadingComplete, setLoadingComplete] = useState(false);
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
 
-	useEffect(() => {
-		AdMobInterstitial.setAdUnitID("ca-app-pub-5832084307445472/7196223658"); // Test ID, Replace with your-admob-unit-id
-		AdMobInterstitial.setTestDeviceID("EMULATOR");
+  InAppPurchases.setPurchaseListener(({ responseCode, results, errorCode }) => {
+    // Purchase was successful
+    if (responseCode === InAppPurchases.IAPResponseCode.OK) {
+      results.forEach((purchase) => {
+        if (!purchase.acknowledged) {
+          console.log(`Successfully purchased ${purchase.productId}`);
+          // Process transaction here and unlock content...
+          AdMobInterstitial.setAdUnitID(null);
+          setHistory(results);
 
-		setInterval(async () => {
-			const ad = await AdMobInterstitial.requestAdAsync();
-			AdMobInterstitial.showAdAsync();
-		}, 1800000);
-	}, []);
+          // Then when you're done
+          InAppPurchases.finishTransactionAsync(purchase, true);
+        }
+      });
+    }
 
-	if (WebView.defaultProps == null) WebView.defaultProps = {};
-	WebView.defaultProps.useWebKit = true;
+    // Else find out what went wrong
+    if (responseCode === InAppPurchases.IAPResponseCode.USER_CANCELED) {
+      console.log("User canceled the transaction");
+    } else if (responseCode === InAppPurchases.IAPResponseCode.DEFERRED) {
+      console.log(
+        "User does not have permissions to buy but requested parental approval (iOS only)"
+      );
+    } else {
+      console.warn(
+        `Something went wrong with the purchase. Received errorCode ${errorCode}`
+      );
+    }
+  });
 
-	if (!isLoadingComplete && !props.skipLoadingScreen) {
-		return (
-			<AppLoading
-				startAsync={loadResourcesAsync}
-				onError={handleLoadingError}
-				onFinish={() => handleFinishLoading(setLoadingComplete)}
-			/>
-		);
-	} else {
-		return (
-			<View style={styles.container}>
-				{Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
-				<AppNavigator />
-			</View>
-		);
-	}
+  const [history, setHistory] = useState(undefined);
+
+  initPurchases = async () => {
+    const response = await InAppPurchases.connectAsync();
+    if (response.responseCode === InAppPurchases.IAPResponseCode.OK) {
+      setHistory(response.results);
+      if (
+        response.results[0].purchaseState ===
+        InAppPurchases.InAppPurchaseState.PURCHASED
+      ) {
+        AdMobInterstitial.setAdUnitID(null); // Test ID, Replace with your-admob-unit-id
+      } else {
+        AdMobInterstitial.setAdUnitID("ca-app-pub-5832084307445472/7196223658"); // Test ID, Replace with your-admob-unit-id
+        AdMobInterstitial.setTestDeviceID("EMULATOR");
+      }
+    }
+  };
+
+  useEffect(() => {
+    try {
+      loadResourcesAsync().then(() => {
+        handleFinishLoading(setLoadingComplete);
+      });
+    } catch (error) {
+      handleLoadingError(error);
+    }
+
+    setInterval(async () => {
+      const ad = await AdMobInterstitial.requestAdAsync();
+      AdMobInterstitial.showAdAsync();
+    }, 180000);
+  }, []);
+
+  if (WebView.defaultProps == null) WebView.defaultProps = {};
+  WebView.defaultProps.useWebKit = true;
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return <View></View>;
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
+        <AppNavigator screenProps={{ history: history }} />
+      </View>
+    );
+  }
 }
 
 async function loadResourcesAsync() {
-	let info = await FileSystem.getInfoAsync(
-		FileSystem.documentDirectory + "posts"
-	);
-	if (!info.exists) {
-		FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + "posts");
-	}
-	await Promise.all([
-		Asset.loadAsync([
-			require("./assets/images/icon.png"),
-			require("./assets/images/splash.png")
-		]),
-		Font.loadAsync({
-			// This is the font that we are using for our tab bar
-			...Ionicons.font,
-			// We include SpaceMono because we use it in HomeScreen.js. Feel free to
-			// remove this if you are not using it in your app
-			"space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
-		})
-	]);
+  initPurchases();
+  let info = await FileSystem.getInfoAsync(
+    FileSystem.documentDirectory + "posts"
+  );
+  if (!info.exists) {
+    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + "posts");
+  }
+  await Promise.all([
+    Asset.loadAsync([
+      require("./assets/images/icon.png"),
+      require("./assets/images/splash.png"),
+    ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Ionicons.font,
+      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+      // remove this if you are not using it in your app
+      "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
+    }),
+  ]);
 }
 
 function handleLoadingError(error) {
-	// In this case, you might want to report the error to your error reporting
-	// service, for example Sentry
-	console.warn(error);
+  // In this case, you might want to report the error to your error reporting
+  // service, for example Sentry
+  console.warn(error);
 }
 
 function handleFinishLoading(setLoadingComplete) {
-	setLoadingComplete(true);
+  setLoadingComplete(true);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff"
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
 });
--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import {
-	View,
-	Text,
-	StyleSheet,
-	StatusBar,
-	AsyncStorage,
-	ScrollView,
-	Image,
-	Switch,
-	Share,
-	Platform
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  AsyncStorage,
+  ScrollView,
+  Image,
+  Switch,
+  Share,
+  Platform,
+  ActivityIndicator,
 } from "react-native";
 import Dimensions from "../constants/Layout";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -18,231 +19,298 @@ import { Linking } from "expo";
 import * as FileSystem from "expo-file-system";
 import * as StoreReview from "expo-store-review";
 
+import * as InAppPurchases from "expo-in-app-purchases";
+
 export default function ContactScreen(props) {
-	return (
-		<View style={[styles.container]}>
-			<View style={styles.headerContainer}>
-				<Text style={styles.header}>Daily Inspired</Text>
-			</View>
+  const [loading, setLoading] = useState(false);
 
-			<ScrollView>
-				<View style={{ width: Dimensions.window.width, height: 250 }}>
-					<Image
-						source={{
-							uri:
-								"http://bibleinspirationdaily.online/wp-content/uploads/2019/12/forest-mountains-fog-clouds-9754.jpg"
-						}}
-						style={{ flex: 1, width: null, height: null, resizeMode: "cover" }}
-					></Image>
-				</View>
-				<View
-					style={{
-						paddingHorizontal: 20,
-						marginTop: 20
-					}}
-				>
-					<Text>
-						We’re here to inspire you daily with the word or God. Daily Bible
-						Devotionals, help you grow in your faith, and stay connected more to
-						Jesus. Also, today’s main stream media often portrays negative news,
-						so we created a “positive news” section that only portrays positive
-						news and articles, we hope that they will bring encouragement to
-						your life.
-					</Text>
-				</View>
-				<View
-					style={{
-						justifyContent: "center",
-						marginTop: 20,
-						marginHorizontal: 20
-					}}
-				>
-					<TouchableOpacity
-						activeOpacity={0.8}
-						onPress={() => {
-							try {
-								StoreReview.requestReview();
-							} catch (error) {
-								alert(error);
-							}
-						}}
-						style={styles.button}
-					>
-						<TabBarIcon
-							name={Platform.OS === "ios" ? "ios-appstore" : "md-appstore"}
-						></TabBarIcon>
-						<Text style={styles.buttonText}>Review on the App Store</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={async () => {
-							try {
-								const result = await Share.share({
-									title: "Inspired Daily",
-									url: "http://bibleinspirationdaily.online/"
-								});
+  const [items, setItems] = useState(undefined);
 
-								if (result.action === Share.sharedAction) {
-									if (result.activityType) {
-										// shared with activity type of result.activityType
-									} else {
-										// shared
-									}
-								} else if (result.action === Share.dismissedAction) {
-									// dismissed
-								}
-							} catch (error) {
-								alert(error.message);
-							}
-						}}
-						style={styles.button}
-					>
-						<TabBarIcon
-							name={Platform.OS === "ios" ? "ios-share-alt" : "md-share-alt"}
-						></TabBarIcon>
-						<Text style={styles.buttonText}>Tell a Friend</Text>
-					</TouchableOpacity>
-				</View>
-				<View
-					style={{
-						justifyContent: "center",
-						marginTop: 20,
-						marginHorizontal: 20
-					}}
-				>
-					<Text>
-						If you have any suggestions or concerns, make sure to contact us via
-					</Text>
-				</View>
-				<View
-					style={{
-						justifyContent: "center",
-						marginTop: 20,
-						marginHorizontal: 20
-					}}
-				>
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL("mailto:bibleinspirationdaily@gmail.com");
-						}}
-						activeOpacity={0.8}
-						style={styles.button}
-					>
-						<TabBarIcon
-							name={Platform.OS === "ios" ? "ios-mail" : "md-mail"}
-						></TabBarIcon>
-						<Text style={styles.buttonText}>Send us an Email </Text>
-					</TouchableOpacity>
-				</View>
-				<View>
-					<Text style={{ textAlign: "center", marginBottom: 10 }}>
-						Follow us on Social Media
-					</Text>
+  useEffect(() => {
+    initItems = async () => {
+      const items = Platform.select({
+        ios: ["removeAds"],
+        android: ["gas", "premium", "gold_monthly", "gold_yearly"],
+      });
 
-					<View
-						style={{
-							flexDirection: "row",
-							justifyContent: "space-between",
-							paddingHorizontal: 100,
-							marginBottom: 10
-						}}
-					>
-						<TouchableOpacity
-							onPress={() => {
-								Linking.openURL("https://www.instagram.com/dailyinspiredapp/");
-							}}
-						>
-							<TabBarIcon
-								name={
-									Platform.OS === "ios" ? "logo-instagram" : "logo-instagram"
-								}
-								size={50}
-								color="rgba(0,0,0,0.4)"
-							></TabBarIcon>
-						</TouchableOpacity>
-						<TouchableOpacity
-							onPress={() => {
-								Linking.openURL("https://www.facebook.com/dailyinspiredapp/");
-							}}
-						>
-							<TabBarIcon
-								name={Platform.OS === "ios" ? "logo-facebook" : "logo-facebook"}
-								size={50}
-								color="rgba(0,0,0,0.4)"
-							></TabBarIcon>
-						</TouchableOpacity>
-					</View>
-				</View>
-				<View
-					style={{
-						borderTopColor: "rgba(0,0,0,0.1)",
-						borderTopWidth: StyleSheet.hairlineWidth,
-						justifyContent: "center",
-						paddingTop: 20,
-						marginHorizontal: 20
-					}}
-				>
-					<TouchableOpacity
-						style={[styles.button, { backgroundColor: "red" }]}
-						onPress={async () => {
-							let info = await FileSystem.getInfoAsync(
-								FileSystem.documentDirectory + "posts"
-							);
-							if (info.exists) {
-								FileSystem.deleteAsync(FileSystem.documentDirectory + "posts/");
-								AsyncStorage.removeItem("saved");
-								AsyncStorage.removeItem("savedIds");
-								FileSystem.makeDirectoryAsync(
-									FileSystem.documentDirectory + "posts"
-								);
-							}
-							alert("Posts Deleted");
-						}}
-					>
-						<TabBarIcon
-							name={Platform.OS === "ios" ? "ios-trash" : "md-trash"}
-						></TabBarIcon>
-						<Text style={styles.buttonText}>Delete saved Posts</Text>
-					</TouchableOpacity>
-				</View>
-			</ScrollView>
-		</View>
-	);
+      // Retrieve product details
+      const { responseCode, results } = await InAppPurchases.getProductsAsync(
+        items
+      );
+      if (responseCode === InAppPurchases.IAPResponseCode.OK) {
+        setItems(results);
+      }
+    };
+
+    initItems();
+  }, []);
+
+  const [history, setHistory] = useState(props.screenProps.history);
+
+  // useEffect(() => {
+  //   getHistory = async () => {
+  //     const { responseCode, results } = InAppPurchases.getPurchaseHistoryAsync(
+  //       true
+  //     );
+  //     if (responseCode === InAppPurchases.IAPResponseCode.OK) {
+  //       setHistory(results);
+  //     }
+  //   };
+
+  //   getHistory();
+  // }, []);
+
+  return (
+    <View style={[styles.container]}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Daily Inspired</Text>
+      </View>
+
+      <ScrollView>
+        <View style={{ width: Dimensions.window.width, height: 250 }}>
+          <Image
+            source={{
+              uri:
+                "http://bibleinspirationdaily.online/wp-content/uploads/2019/12/forest-mountains-fog-clouds-9754.jpg",
+            }}
+            style={{ flex: 1, width: null, height: null, resizeMode: "cover" }}
+          ></Image>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: 20,
+            marginTop: 20,
+          }}
+        >
+          <Text>
+            We’re here to inspire you daily with the word or God. Daily Bible
+            Devotionals, help you grow in your faith, and stay connected more to
+            Jesus. Also, today’s main stream media often portrays negative news,
+            so we created a “positive news” section that only portrays positive
+            news and articles, we hope that they will bring encouragement to
+            your life.
+          </Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "center",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <TouchableOpacity
+            disabled={history === undefined}
+            style={
+              history === undefined
+                ? [styles.button, { backgroundColor: "gray" }]
+                : styles.button
+            }
+            onPress={async () => {
+              setLoading(true);
+              InAppPurchases.purchaseItemAsync(items[0].productId).then(() => {
+                setLoading(false);
+              });
+            }}
+          >
+            {loading ? (
+              <ActivityIndicator></ActivityIndicator>
+            ) : (
+              <>
+                <TabBarIcon
+                  name={
+                    Platform.OS === "ios" ? "ios-backspace" : "md-backspace"
+                  }
+                ></TabBarIcon>
+                <Text style={styles.buttonText}>Remove Ads</Text>
+              </>
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => {
+              try {
+                StoreReview.requestReview();
+              } catch (error) {
+                alert(error);
+              }
+            }}
+            style={styles.button}
+          >
+            <TabBarIcon
+              name={Platform.OS === "ios" ? "ios-appstore" : "md-appstore"}
+            ></TabBarIcon>
+            <Text style={styles.buttonText}>Review on the App Store</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={async () => {
+              try {
+                const result = await Share.share({
+                  title: "Inspired Daily",
+                  url: "http://bibleinspirationdaily.online/",
+                });
+
+                if (result.action === Share.sharedAction) {
+                  if (result.activityType) {
+                    // shared with activity type of result.activityType
+                  } else {
+                    // shared
+                  }
+                } else if (result.action === Share.dismissedAction) {
+                  // dismissed
+                }
+              } catch (error) {
+                alert(error.message);
+              }
+            }}
+            style={styles.button}
+          >
+            <TabBarIcon
+              name={Platform.OS === "ios" ? "ios-share-alt" : "md-share-alt"}
+            ></TabBarIcon>
+            <Text style={styles.buttonText}>Tell a Friend</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            justifyContent: "center",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <Text>
+            If you have any suggestions or concerns, make sure to contact us via
+          </Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "center",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              Linking.openURL("mailto:bibleinspirationdaily@gmail.com");
+            }}
+            activeOpacity={0.8}
+            style={styles.button}
+          >
+            <TabBarIcon
+              name={Platform.OS === "ios" ? "ios-mail" : "md-mail"}
+            ></TabBarIcon>
+            <Text style={styles.buttonText}>Send us an Email </Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={{ textAlign: "center", marginBottom: 10 }}>
+            Follow us on Social Media
+          </Text>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingHorizontal: 100,
+              marginBottom: 10,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                Linking.openURL("https://www.instagram.com/dailyinspiredapp/");
+              }}
+            >
+              <TabBarIcon
+                name={
+                  Platform.OS === "ios" ? "logo-instagram" : "logo-instagram"
+                }
+                size={50}
+                color="rgba(0,0,0,0.4)"
+              ></TabBarIcon>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                Linking.openURL("https://www.facebook.com/dailyinspiredapp/");
+              }}
+            >
+              <TabBarIcon
+                name={Platform.OS === "ios" ? "logo-facebook" : "logo-facebook"}
+                size={50}
+                color="rgba(0,0,0,0.4)"
+              ></TabBarIcon>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            borderTopColor: "rgba(0,0,0,0.1)",
+            borderTopWidth: StyleSheet.hairlineWidth,
+            justifyContent: "center",
+            paddingTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "red" }]}
+            onPress={async () => {
+              let info = await FileSystem.getInfoAsync(
+                FileSystem.documentDirectory + "posts"
+              );
+              if (info.exists) {
+                FileSystem.deleteAsync(FileSystem.documentDirectory + "posts/");
+                AsyncStorage.removeItem("saved");
+                AsyncStorage.removeItem("savedIds");
+                FileSystem.makeDirectoryAsync(
+                  FileSystem.documentDirectory + "posts"
+                );
+              }
+              alert("Posts Deleted");
+            }}
+          >
+            <TabBarIcon
+              name={Platform.OS === "ios" ? "ios-trash" : "md-trash"}
+            ></TabBarIcon>
+            <Text style={styles.buttonText}>Delete saved Posts</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 ContactScreen.navigationOptions = {
-	header: null
+  header: null,
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		paddingTop: 15,
-		backgroundColor: "#fff",
-		paddingTop: StatusBar.currentHeight + 20 || 40
-	},
-	header: {
-		fontSize: 35,
-		textAlign: "center",
-		fontWeight: Platform.OS === "ios" ? "900" : "700"
-	},
-	headerContainer: {
-		width: "100%",
-		borderBottomWidth: StyleSheet.hairlineWidth,
-		borderBottomColor: "rgba(0,0,0,0.1)",
-		paddingBottom: 20
-	},
-	button: {
-		alignSelf: "stretch",
-		backgroundColor: "steelblue",
-		padding: 10,
-		marginBottom: 20,
-		alignItems: "center",
-		borderRadius: 15,
-		flexDirection: "row",
-		justifyContent: "center"
-	},
-	buttonText: {
-		marginLeft: 10,
-		color: "white"
-	}
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight + 20 || 40,
+  },
+  header: {
+    fontSize: 35,
+    textAlign: "center",
+    fontWeight: Platform.OS === "ios" ? "900" : "700",
+  },
+  headerContainer: {
+    width: "100%",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "rgba(0,0,0,0.1)",
+    paddingBottom: 20,
+  },
+  button: {
+    alignSelf: "stretch",
+    backgroundColor: "steelblue",
+    padding: 10,
+    marginBottom: 20,
+    alignItems: "center",
+    borderRadius: 15,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonText: {
+    marginLeft: 10,
+    color: "white",
+  },
 });
