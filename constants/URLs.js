1698aa82218878bb65af38533d06c89ee9ef3dfe
Wahaj Hussain <wahajhssn@gmail.com>
2020-07-15 21:25:57 -0700

    URLs

---
+++ constants/URLs.js
@@ -0,0 +1,6 @@
+export default {
+	facebook: "https://www.facebook.com/trustgodhub ",
+	instagram: "https://www.instagram.com/trustgodhub/ ",
+	twitter: "https://twitter.com/trustgodhub ",
+	youtube: "https://www.youtube.com/channel/UCvTCFhLcXmVIRp-ANj1MuAA",
+};
